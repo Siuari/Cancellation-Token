bd5644552faa10bbb312c87cfff2ff87763f4a76
Siuari <siuaridamaceno@gmail.com>
2024-03-22 13:56:04 -0300

    Projeto para estudo de cancellation token

---
+++ DataServerApi/Controllers/CancellationTokenTestController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataServerApi.Controllers;
+
+[Route("teste")]
+public class CancellationTokenTestController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public CancellationTokenTestController()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    [HttpGet("propagando")]
+    public async Task<IActionResult> TestePropagando(CancellationToken cancellationToken)
+    {
+        var url = new Uri("http://localhost:5182/cancellation-token/propagado");
+
+        await _httpClient.GetAsync(url, cancellationToken);
+        
+        return Ok();
+    }
+
+    [HttpGet("nao-propagado")]
+    public async Task<IActionResult> TesteNaoPropagando(CancellationToken cancellationToken)
+    {
+        var url = new Uri("http://localhost:5182/cancellation-token/nao-propagado");
+
+        await _httpClient.GetAsync(url, cancellationToken);
+        
+        return Ok();
+    }
+}
---
+++ WebApi/Controllers/CancellationTokenController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("cancellation-token")]
+public class CancellationTokenController : ControllerBase
+{
+    [HttpGet("propagado")]
+    public async Task<IActionResult> CancellationTokenPropagado(CancellationToken cancellationToken)
+    {
+        var segundos = 1;
+        
+        while (true)
+        {
+            Console.WriteLine(segundos++  + " Segundos");
+            
+            await Task.Delay(1000, cancellationToken);
+            
+            if(segundos > 10) break;
+        }
+
+        return Ok();
+    }
+    
+    [HttpGet("nao-propagado")]
+    public async Task<IActionResult> CancellationNaoTokenPropagado(CancellationToken cancellationToken)
+    {
+        var segundos = 1;
+        
+        while (true)
+        {
+            Console.WriteLine($"{segundos++} Segundos");
+            
+            await Task.Delay(1000);
+            
+            if(segundos > 10) break;
+        }
+
+        return Ok();
+    }
+    
+    [HttpGet("controlado-manualmente")]
+    public async Task<IActionResult> GetManualmente(CancellationToken cancellationToken)
+    {
+        var segundos = 1;
+        
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Console.WriteLine($"{segundos++} Segundos");
+            
+            await Task.Delay(1000);
+            
+            if(segundos > 10) break;
+        }
+
+        if (cancellationToken.IsCancellationRequested) Console.WriteLine("Requisição Cancelada");
+
+        return Ok();
+    }
+}
